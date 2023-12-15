c8af1cc8d1d0fbcdf82e00521f1d092e85acc29b
roupecraphael <127327904+roupecraphael@users.noreply.github.com>
2023-12-15 17:51:19 +0100

    Added Doc where missing

--- app/src/main/java/at/ac/fhcampuswien/temperatureexample/MainActivity.kt
+++ app/src/main/java/at/ac/fhcampuswien/temperatureexample/MainActivity.kt
@@ -1,5 +1,5 @@
 package at.ac.fhcampuswien.temperatureexample
-// import all necessary external libraries here ----------------------------------------------------------------------------//
+// import all necessary external libraries here -------------------------------------------------------------------------------//
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -18,28 +18,28 @@ import at.ac.fhcampuswien.temperatureexample.data.LebensmittelData
 import at.ac.fhcampuswien.temperatureexample.data.LebensmittelDatabase
 import at.ac.fhcampuswien.temperatureexample.databinding.ActivitySecondBinding
 import at.ac.fhcampuswien.temperatureexample.databinding.ActivityMainBinding
-// this is the main activity class that is called when the app is started --------------------------------------------------//
+// this is the main activity class that is called when the app is started -----------------------------------------------------//
 class MainActivity : AppCompatActivity() {
-    // Private variables are declared here -------------------------------------------------------------------------------------//
+    // Private variables are declared here ------------------------------------------------------------------------------------//
     private lateinit var binding: ActivityMainBinding
     private var locations = listOf<LebensmittelData>()
     private var index = 0
-    // overide function is called when the app is started ----------------------------------------------------------------------//
+    // overide function is called when the app is started ---------------------------------------------------------------------//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// utilizes the databinding library to bind the layout to the activity -----------------------------------------------------//
+// utilizes the databinding library to bind the layout to the activity --------------------------------------------------------//
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-// the following lines are used to bind the layout elements to the activity variables --------------------------------------//
+// the following lines are used to bind the layout elements to the activity variables -----------------------------------------//
         val LebensmittelView: EditText = findViewById(R.id.lebensmittel)
         val GewichtView: EditText = findViewById(R.id.gewicht)
         val KohlenhydrateView: EditText = findViewById(R.id.kohlenhydrate)
-//the following values are used to create a database and insert data into it -----------------------------------------------//
+//the following values are used to create a database and insert data into it --------------------------------------------------//
         val db = LebensmittelDatabase.getInstance(applicationContext)
         val dao = db.temperatureDao
         locations = dao.getAllLocations()
         binding.temp = locations[0]
-// the following lines create a val that uses the button id to find the button and then set an onclick listener to change --//
-// to a second activity when the button is pressed -------------------------------------------------------------------------//
+// the following lines create a val that uses the button id to find the button and then set an onclick listener to change -----//
+// to a second activity when the button is pressed ----------------------------------------------------------------------------//
         val goToAnotherClass: Button
         goToAnotherClass =
             findViewById<View>(R.id.showdata) as Button //find the button by its assigned id
@@ -50,8 +50,8 @@ class MainActivity : AppCompatActivity() {
             )
             startActivity(myIntent)
         }
-// the following lines create a val that uses the button id to find the button and then set an onclick listener to  --------//
-// add data from the edit text fields to the database ----------------------------------------------------------------------//
+// the following lines create a val that uses the button id to find the button and then set an onclick listener to  -----------//
+// add data from the edit text fields to the database -------------------------------------------------------------------------//
         val addDatatodatabase: Button
         addDatatodatabase = findViewById(R.id.adddata) as Button
         addDatatodatabase.setOnClickListener {
@@ -64,8 +64,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-// the following class is used to create a second activity that is called when the button is pressed and displays the data -//
-// from the database in a recycler view  -----------------------------------------------------------------------------------//
+// the following class is used to create a second activity that is called when the button is pressed and displays the data ----//
+// from the database in a recycler view  --------------------------------------------------------------------------------------//
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,31 +79,31 @@ class SecondActivity : AppCompatActivity() {
         view.layoutManager = LinearLayoutManager(this)
     }
 }
-// the following class Viewholder is used to bind the R.id.lebensmittelid, R.id.gewichtid and R.id.kohlenhydrateid to  -----//
-// variables so they can be used by recycler view in the second activity ---------------------------------------------------//
+// the following class Viewholder is used to bind the R.id.lebensmittelid, R.id.gewichtid and R.id.kohlenhydrateid to  --------//
+// variables so they can be used by recycler view in the second activity ------------------------------------------------------//
 class LebensmittelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val lebensmittelName: TextView = itemView.findViewById(R.id.lebensmittelid)
     val gewicht: TextView = itemView.findViewById(R.id.gewichtid)
     val kohlenhydrate: TextView = itemView.findViewById(R.id.kohlenhydrateid)
 }
-// the following class is used to create an adapter that is used by the ViewHolder to update the display of the data -------//
-// from the database in the second activity --------------------------------------------------------------------------------//
+// the following class is used to create an adapter that is used by the ViewHolder to update the display of the data ----------//
+// from the database in the second activity -----------------------------------------------------------------------------------//
 class LebensmittelAdapter(private val lebensmittelList: List<LebensmittelData>) :
     RecyclerView.Adapter<LebensmittelViewHolder>() {
-    // the following functions are used to create the ViewHolder ---------------------------------------------------------------//
+    // the following functions are used to create the ViewHolder --------------------------------------------------------------//
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LebensmittelViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_database, parent, false)
         return LebensmittelViewHolder(view)
     }
-    // the following functions are used to bind the data from the database to the ViewHolder -----------------------------------//
+    // the following functions are used to bind the data from the database to the ViewHolder ----------------------------------//
     override fun onBindViewHolder(holder: LebensmittelViewHolder, position: Int) {
         val lebensmitteln = lebensmittelList[position]
         holder.lebensmittelName.text = lebensmitteln.lebensmittel
         holder.gewicht.text = lebensmitteln.gewicht.toString()
         holder.kohlenhydrate.text  = lebensmitteln.kohlenhydrate.toString()
     }
-    // the following function is used to get the number of items in the database -----------------------------------------------//
+    // the following function is used to get the number of items in the database ----------------------------------------------//
     override fun getItemCount() = lebensmittelList.size
 }
 
--- app/src/main/java/at/ac/fhcampuswien/temperatureexample/data/LebensmittelDao.kt
+++ app/src/main/java/at/ac/fhcampuswien/temperatureexample/data/LebensmittelDao.kt
@@ -1,31 +1,31 @@
 package at.ac.fhcampuswien.temperatureexample.data
-
+// import all necessary external libraries here --------------------------------------------------------------------------------//
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-
+// @Dao annotation marks the class as a Room database Dao ---------------------------------------------------------------------//
 @Dao
 interface LebensmittelDao {
-
+// the following lines are used to create a function that is used to insert data into the database ------------------------------//
     @Insert
     fun insert(data: LebensmittelData)
-
+// the following lines are used to create a function that is used to update data in the database -------------------------------//
     @Update
     fun update(data: LebensmittelData)
-
+// the following lines are used to create a function that is used to get data from the database --------------------------------//
     @Query("SELECT * FROM lebensmittel_data WHERE dataId = :key")
     fun getById(key: Long): LebensmittelData?
-
+// the following lines are used to create a function that is used to get lebensmittel data by id from the database -------------//
     @Query("SELECT * FROM lebensmittel_data WHERE lebensmittel = :lebensmittel")
     fun getByLebensmittel(lebensmittel: String): LebensmittelData?
-
+// the following lines are used to create a function that is used to get all data from the database -----------------------------//
     @Query("SELECT * FROM lebensmittel_data")
     fun getAllLocations(): List<LebensmittelData>
-
+// the following lines are used to create a function that is used to delete data from the database ------------------------------//
     @Query("DELETE from lebensmittel_data WHERE dataId= :key")
     fun delete(key:Long): Int
-
+// the following lines are used to create a function that is used to delete all data from the database --------------------------//
     @Query("DELETE FROM lebensmittel_data")
     fun deleteAll(): Void
 }
